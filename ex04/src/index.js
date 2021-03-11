268cd466a9f34c801f47c2aac7ae88c78a28b25e
EmilJulevic <emiljulevic@live.com>
2021-03-11 18:47:48 +0100

    Adding ex04 folder

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+     myDecimal = 8.2;
+     myDecimalE = 2.54e-3;
+
+
+    return{
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+module.exports = main;
