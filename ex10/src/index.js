ab0c5a4dd18ed7d2636ca0d9828393c1248c3cfd
EmilJulevic <emiljulevic@live.com>
2021-03-11 18:59:04 +0100

    Adding ex10 folder

---
+++ ex10/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+
+var myFirstName = "Emil";
+var myLastName = "Julevic";
+var myHomeTown = "Gusinje";
+
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
